783ebbd9880cc894f372adb9c75dd2ecc696bb50
RyanFaulkenberry100 <ryan.faulkenberry@ttu.edu>
2017-05-04 18:46:22 -0500

    Only one library exists: the one with measured error bounds

--- RealNumberLibrary.py
+++ RealNumberLibrary.py
@@ -1,9 +1,9 @@
 ''' This file defines arithmetic functions over real numbers with rates of
-    convergence, called "Bishop numbers."
+    convergence, called measured real numbers
 
     A real number is a convergent sequence of rational numbers.
     A rational number is a python fraction, as defined in the fraction class.    
-    A Bishop number is a pair (x, k(eps)) where x is a real number that
+    A measured real number is a pair (x, k(eps)) where x is a real number that
         converges to its limit at rate k(eps).
     k(eps) is a function from positive rational numbers to positive integers.
         Where eps is an error bound for a real number, and for every integer
@@ -41,15 +41,21 @@
 from fractions import Fraction as fr
 from math import factorial as fact
 from math import gcd
+from math import ceil
 
-# If r is a rational number, bish(r) is a Bishop number corresponding to r
+# For ease of testing
+a = (lambda n: 1/n, lambda eps: ceil(1/eps))
+b = (lambda n: 1/n**2, lambda eps: ceil((1/eps)**.5))
+
+# If r is a rational number, bish(r) is a measured real number corresponding
+# to r
 def bish(r):
     def x(n): return r
     def k(eps): return 0
     return (x, k)
 
-# If a and b are bishop numbers, a[0]->L at rate a[1] and b[0]->M at rate b[1],
-# then addB(a, b)->L+M at rate k'
+# If a and b are measured real numbers, a[0]->L at rate a[1] and b[0]->M at
+# rate b[1], then addB(a, b)->L+M at rate k'
 def addB(a, b):
     x, j = a
     y, k = b
@@ -65,53 +71,86 @@ def addB(a, b):
         
     return (z, kPrime)
 
-# If a and b are bishop numbers, a[0]->L at rate a[1] and b[0]->M at rate b[1],
-# then subB(a, b)->L-M at rate k'
+# If a and b are measured real numbers, a[0]->L at rate a[1] and b[0]->M at
+# rate b[1], then subB(a, b)->L-M at rate k'
 def subB(a, b):
     x, j = a
     y, k = b
     
     def z(n):
         try:
-            return a(n) - b(n)
+            return x(n) - y(n)
         except ZeroDivisionError:
             return fr(0,1)
 
-    def kPrime(eps):
-        return x(n) + y(n)
+    def kPrime(eps): return j(eps) + k(eps)
     
     return (z, kPrime)
 
-# If a and b are bishop numbers, a[0]->L at rate a[1] and b[0]->M at rate b[1],
-# then mulB(a, b)->LM at rate k'
+# If a and b are measured real numbers, a[0]->L at rate a[1] and b[0]->M at
+# rate b[1], then mulB(a, b)->LM at rate k'
 def mulB(a, b):
     x, j = a
     y, k = b
     
     def z(n):
         try:
-            return a(n) * b(n)
+            return x(n) * y(n)
         except ZeroDivisionError:
             return fr(0,1)
 
-    def kPrime(eps):
-        #return x(n) + y(n)
+    def kPrime(eps): return ceil(x(eps) * y(eps)) # Inefficient (but correct)
     
     return (z, kPrime)
 
-# If a and b are bishop numbers, a[0]->L at rate a[1], b[0]->M at rate b[1], and
-# M != 0, then divB(a, b)->L/M at rate k'
+# If a and b are measured real numbers, a[0]->L at rate a[1], b[0]->M at rate
+# b[1], and M != 0, then divB(a, b)->L/M at rate k'
 def divB(a, b):
     x, j = a
     y, k = b
     
     def z(n):
         try:
-            return a(n) / b(n)
+            return x(n) / y(n)
         except ZeroDivisionError:
             return fr(0,1)
 
-    def kPrime(eps):
-        #return x(n) + y(n)
+    def kPrime(eps): return ceil(x(eps) * y(eps)) # Inefficient (but correct)
     
     return (z, kPrime)
+
+# If a is a measured real number and a[0]->L at rate a[1], then sinB(a)->sine(a)
+# at rate k', where sine(a) is the true sine of a
+def sinB(a):
+    x, j = a
+    def Q(n): return sinQ(x(n), fr(1,n))
+    def kPrime(eps): return j(eps)
+    return (Q, kPrime)
+
+# If x is a rational number and eps is a positive rational, then
+# |sinQ(x, eps) - sine(x)| < eps, where sine(x) is the true sine of x
+def sinQ(q, eps):
+    n = R(q, eps)
+    return P(n, q)
+
+# If x is a rational number and eps is a positive rational, R(q, eps) returns an
+# n to be used for P(n, x) st |P(n, q) - sin(q)| < eps, per Taylor's Theorem
+def R(q, eps):
+    n = 0
+    R = eps + 1 #Something always greater than eps
+    while (R >= eps):
+        R = q**(n+1) * fr(1, fact(n+1))
+        n += 1
+    return n
+
+# If q is a rational and n is a positive int, P(n, q) is the summation
+# from k=0 to n of (sine^k(0)*(q**k)) / k!, per Taylor's Theorem
+def P(n, q):
+    PSum = fr(0,1)
+    for k in range(0, n):
+        PSum += q * fr(sinDeriv(k), fact(k))
+    return PSum
+    
+# if n is a nonnegative integer, sinDeriv returns the nth derivative of sin(0)
+def sinDeriv(n):
+    return [0, 1, 0, -1][n%4]
